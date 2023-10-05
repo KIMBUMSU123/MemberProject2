b583856980bbe1290faae24b5bf396667b029f09
kimbumsu <test@hanmail.net>
2023-10-05 10:55:40 +0900

    글 수정 완료

--- SpringBoot/board_20231004/src/main/java/com/icia/board/controller/BoardController.java
+++ SpringBoot/board_20231004/src/main/java/com/icia/board/controller/BoardController.java
@@ -28,43 +28,75 @@ public class BoardController {
         boardService.save(boardDTO);
         return "boardPages/boardList";
     }
+    // 게시판 목록을 조회하고, 뷰에 데이터를 전달하는 핸들러
     @GetMapping
     public String findAll(Model model) {
+        // 게시판 서비스를 통해 모든 게시판 데이터를 가져옵니다.
         List<BoardDTO> boardDTOList = boardService.findAll();
+
+        // 모델에 "boardList"라는 이름으로 게시판 목록 데이터를 추가합니다.
         model.addAttribute("boardList", boardDTOList);
+
+        // "boardPages/boardList" 뷰로 이동합니다.
         return "boardPages/boardList";
     }
 
+    // 특정 게시물의 세부 정보를 조회하고, 뷰에 데이터를 전달하는 핸들러
     @GetMapping("/{id}")
     public String findById(@PathVariable("id") Long id, Model model) {
+        // 게시물 조회수를 증가시킵니다.
         boardService.increaseHits(id);
+
+        // 게시물 서비스를 통해 특정 ID에 해당하는 게시물 데이터를 가져옵니다.
         BoardDTO boardDTO = boardService.findById(id);
+
+        // 모델에 "board"라는 이름으로 게시물 데이터를 추가합니다.
         model.addAttribute("board", boardDTO);
+
+        // "boardPages/boardDetail" 뷰로 이동합니다.
         return "boardPages/boardDetail";
     }
 
-    // 주소로 요청
+    // 주소를 통해 게시물을 삭제하는 핸들러
     @GetMapping("/delete/{id}")
     public String delete(@PathVariable("id") Long id){
+        // 게시물 서비스를 통해 특정 ID에 해당하는 게시물을 삭제합니다.
         boardService.delete(id);
+
+        // "/board" 경로로 리디렉션하여 게시판 목록 페이지로 이동합니다.
         return "redirect:/board";
     }
 
-    // axios로 delete 요청
+    // Axios를 통해 DELETE 요청을 처리하는 핸들러
     @DeleteMapping("/{id}")
     public ResponseEntity deleteByAxios(@PathVariable("id") Long id) {
+        // 게시물 서비스를 통해 특정 ID에 해당하는 게시물을 삭제합니다.
         boardService.delete(id);
+
+        // HTTP 응답으로 성공 상태 코드 HttpStatus.OK를 반환합니다.
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    // 게시물을 수정하는 페이지로 이동하는 핸들러
     @GetMapping("/update/{id}")
     public String update(@PathVariable("id") Long id, Model model) {
+        // 게시물 서비스를 통해 특정 ID에 해당하는 게시물 데이터를 가져옵니다.
         BoardDTO boardDTO = boardService.findById(id);
+
+        // 모델에 "board"라는 이름으로 게시물 데이터를 추가합니다.
         model.addAttribute("board", boardDTO);
+
+        // "boardPages/boardUpdate" 뷰로 이동합니다.
         return "boardPages/boardUpdate";
     }
+
+    // 게시물을 수정하는 요청을 처리하는 핸들러
     @PutMapping("/{id}")
     public ResponseEntity update(@RequestBody BoardDTO boardDTO){
+        // 게시물 서비스를 통해 게시물 데이터를 업데이트합니다.
         boardService.update(boardDTO);
+
+        // HTTP 응답으로 성공 상태 코드 HttpStatus.OK를 반환합니다.
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
